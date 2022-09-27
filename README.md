e8a7502d5e0ca36734f374506314a38be22a5195
YunshanJiang <yunshantree@gmail.com>
2022-09-27 11:40:50 +0800

    create project

---
+++ README.md
@@ -0,0 +1,19 @@
+# connection-lab-02
+
+The project i want to make is a calendar website. 
+Intentions:
+My idea is user can customize their own calendar which allow users to add reminder, videos, images and etc. The website give users a space of creation and 
+it is different from social media website that datas are shared to every users. The website creates a private space for users.
+
+
+function:
+The functions of the website have sending daliy quote to user and showing user edited information. Users are able to switch different days and present
+different daily information.
+
+Challenges:
+i have very little knowledge about data space so it would be a chalenge for me to make function like creativing account for users, sending, saving and pulling
+data from users.
+
+
+Refernece:
+https://github.com/uvarov-frontend/vanilla-calendar
---
+++ js/app.js
@@ -0,0 +1,68 @@
+
+let calendar = null;
+let date = new Date();
+let year;
+let month;
+let day;
+
+window.addEventListener("load", function(){
+    fetch('astro.json')
+    .then(response => response.json())
+    .then(data => console.log(data.people))
+    .catch(function(e){
+console.log(e);
+    });
+});
+let dailyInfo = function(year,month,day,imgPath){
+    this.year = year;
+    this.month= month;
+    this.day= day;
+    this.imgPath=imgPath;
+};
+let dailyInfoList = [];
+$( document ).ready(function() {
+    
+    setDate();
+    initializeDailyList();
+    loadData();
+});
+
+
+function setDate(){
+   
+    year = date.getFullYear();
+    month =('0' + (date.getMonth() + 1)).slice(-2);
+    day = ('0' + date.getDate()).slice(-2);
+    $("#date").text(year + '-' + month + '-' + day);
+}
+
+
+function initializeDailyList(){
+    dailyInfoList.push(new dailyInfo("2022","09","18", 
+    ["resource/p1.png","resource/p2.png","resource/p3.png"]));
+    dailyInfoList.push(new dailyInfo("2022","09","19", 
+    ["resource/p1.png","resource/p2.png"]));
+    dailyInfoList.push(new dailyInfo("2022","09","20", 
+    ["resource/p1.png"]));
+   
+}
+
+function loadData(){
+    $("#middle").empty();
+    $("#middle").append("<div ></div>");
+    $("#middle>div").append("<img class='middleImg' src='resource/backGroundWhite.png'>");
+    for (let i = 0;i<dailyInfoList.length;i++){
+        if (dailyInfoList[i].year == year &&
+             dailyInfoList[i].month == month &&
+              dailyInfoList[i].day == day){
+                
+                for (let j = 0;j<dailyInfoList[i].imgPath.length;j++)
+                {
+                    
+                   
+                $("#middle").append("<img class='middleImg' src="
+                 + dailyInfoList[i].imgPath[j] + ">");
+              }
+    }
+}
+}
